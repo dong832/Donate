d75f86fbb1658408922b2ddf6375e38627200a67
dongwankim <kdw6408@naver.com>
2021-08-25 15:42:50 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Reactstrap from './Reactstrap.js';
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div id="wrap">
+        <header id="header">
+          <div class="headInner">
+            <h1>songil</h1>
+            <div class="menuList">
+              <div class="item">커뮤니티</div>
+              <div class="item">후원기업</div>
+            </div>
+          </div>
+        </header>
+      </div>
+
+      <p align="middle">
+        <p>기부해야하는 이유 동영상</p>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/80bNntJbUeg" 
+      title="YouTube video player" frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      allowfullscreen></iframe>
+      </p>
+
+      
+        <div class="box">기부 리스트</div>
+        <div class="box">기부 리스트</div>
+        <div class="box">기부 리스트</div>
+        <div class="box">기부 리스트</div>
+      
+    </body>
   );
 }
 
